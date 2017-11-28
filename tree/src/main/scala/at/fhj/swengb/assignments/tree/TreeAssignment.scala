41d265a0d46a035fca1b4aa21806405646dc5433
MrPink1992 <maximilian.steiner@edu.fh-joanneum.at>
2017-11-28 15:46:04 +0100

    solved traverse function

--- tree/src/main/scala/at/fhj/swengb/assignments/tree/TreeAssignment.scala
+++ tree/src/main/scala/at/fhj/swengb/assignments/tree/TreeAssignment.scala
@@ -39,7 +39,12 @@ object Graph {
     * @param convert a converter function
     * @return
     */
-  def traverse[A, B](tree: Tree[A])(convert: A => B): Seq[B] = ???
+  def traverse[A, B](tree: Tree[A])(convert: A => B): Seq[B] = tree match{
+
+    case Node(v) => Seq(convert(v))
+    case Branch(l, r) => traverse(l)(convert) ++ traverse(r)(convert)
+
+  }
 
 
   /**
