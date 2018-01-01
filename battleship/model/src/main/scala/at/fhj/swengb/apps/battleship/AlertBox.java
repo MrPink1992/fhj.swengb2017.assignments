4c4ddabb18a83430e60fc02c8d8d81ed1fde21c2
JakobArmeitz <Jakob.Arneitz@gmail.com>
2018-01-01 23:46:52 +0100

    Implemented sounds when hitting a target or destroying it completely, and water sounds.

--- battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/AlertBox.java
+++ battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/AlertBox.java
@@ -1,13 +1,21 @@
 package at.fhj.swengb.apps.battleship;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.geometry.*;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.image.Image;
 
+import java.awt.*;
 
 
 public class AlertBox {
@@ -39,23 +47,29 @@ public class AlertBox {
     }
 
     public static void display(String title, String message) {
-        Stage window = new Stage();
+
+
+        Stage window = new Stage(StageStyle.UTILITY);
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
         window.setMinWidth(250);
         window.setFullScreen(true);
         Label label = new Label();
+        label.setPrefSize(1000,300);
+        label.setAlignment(Pos.CENTER);
         label.setText(message);
+        label.setFont(Font.font("Times-New-Roman",20));
         Button closeButton = new Button("Ja");
-        closeButton.setPrefSize(400,400);
+        closeButton.setPrefSize(400,200);
         closeButton.setStyle("-fx-background-color: #add8e6");
-
         closeButton.setOnAction(e -> window.close());
 
-        VBox layout = new VBox(10);
-        layout.getChildren().addAll(label,closeButton);
-        layout.setAlignment(Pos.CENTER);
 
+        VBox layout = new VBox(10);
+        layout.setId("pane");
+        layout.getChildren().addAll(label,closeButton);
+        layout.setBackground(new Background(new BackgroundFill(Color.rgb(17, 119, 255), CornerRadii.EMPTY, Insets.EMPTY)));
+        layout.setAlignment(Pos.CENTER);
         Scene scene = new Scene(layout);
         window.setScene(scene);
         window.showAndWait();
